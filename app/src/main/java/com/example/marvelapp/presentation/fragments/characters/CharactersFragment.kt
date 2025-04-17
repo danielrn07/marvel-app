6fd17e56e591ba71a6c0b75769afe995cffd1c1a
danielrn07 <pdaniel.contato@gmail.com>
2025-04-16 23:37:10 -0300

    feat: add adapter and viewholder

--- app/src/main/java/com/example/marvelapp/presentation/fragments/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/fragments/characters/CharactersFragment.kt
@@ -5,13 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import com.leadmedia.core.domain.model.Character
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding get() = _binding!!
+
+    private val charactersAdapter: CharactersAdapter = CharactersAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ): View {
+        _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+                Character("Daniel", "https://i.pinimg.com/736x/a9/22/03/a92203e1949a17c1f561eb5f71f748e0.jpg"),
+            )
+        )
+    }
+
+    private fun initCharactersAdapter() {
+        binding.recyclerCharacters.run {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ core/src/main/java/com/leadmedia/core/domain/model/Character.kt
@@ -0,0 +1,6 @@
+package com.leadmedia.core.domain.model
+
+data class Character(
+    val name: String,
+    val imageUrl: String
+)
